e11059e70ee232e6d27baa2f41d3024db8a545b0
Alexander Rovang <wolf_vii@yahoo.com>
2021-05-18 20:39:45 -0400

    commit tree 25b33e2848359ceb5876297f71bfbe5ac6073fb5

--- src/App.js
+++ src/App.js
@@ -1,1 +1,21 @@
 // create your App component here
+import React, { Component } from 'react'
+
+export default class App extends Component{
+
+  constructor(){
+    super()
+  }
+
+  componentDidMount(){
+    fetch('http://api.open-notify.org/astros.json')
+    .then(r => r.json())
+    .then(data => {
+      console.log(data)
+    })
+  }
+
+  render(){
+    return <button>hi</button>
+  }
+}
